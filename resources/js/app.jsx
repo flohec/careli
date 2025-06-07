aaa56e6eac0b7d80419c63a5b11f9fbf2e713247
Florian Heck <flohec027@gmail.com>
2025-06-07 11:04:11 +0200

    second

---
+++ resources/js/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Hello from React in Laravels</h1>;
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
